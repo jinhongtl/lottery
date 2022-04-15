036f9e2a7e48c3daea23d65467e4451415cf97c4
金鸿 <jinhong@xizi.com>
2022-04-15 18:00:05 +0800

    make every function from static to normal on interface LotteryInterface

--- src/LotteryInterface.php
+++ src/LotteryInterface.php
@@ -5,20 +5,20 @@ namespace lottery;
 interface LotteryInterface
 {
     /** 进入抽奖前的条件校验*/
-    public static function check($userId, $activityId): bool;
+    public function check($userId, $activityId): bool;
 
     /** 是否允许中奖，包括总抽奖概率与用户中奖次数限制的校验 */
-    public static function checkLimit($userId, $activityId): bool;
+    public function checkLimit($userId, $activityId): bool;
 
     /** 获取满足抽奖条件的奖品 */
-    public static function getPrizes($userId, $activityId): array;
+    public function getPrizes($userId, $activityId): array;
 
     /** 扣除抽奖机会 */
-    public static function deductionLotteryNum($userId, $activityId): bool;
+    public function deductionLotteryNum($userId, $activityId): bool;
 
     /** 预抽奖逻辑 */
-    public static function preLottery($userId, $activityId): bool;
+    public function preLottery($userId, $activityId): bool;
 
     /** 写入奖品 */
-    public static function afterLottery($userId, $activityId, Prize $prize);
+    public function afterLottery($userId, $activityId, Prize $prize);
 }
